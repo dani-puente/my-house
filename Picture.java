ba18f3083bf4dbd6eeaa1f353265ef220ff06576
dani-puente <dpuentet@hotmail.com>
2020-10-14 10:02:46 +0200

    al atardecer aparece una persona que se mueve direccion a la casa

--- Picture.java
+++ Picture.java
@@ -16,6 +16,7 @@ public class Picture
     private Triangle roof;
     private Circle sun;
     private Square campo;
+    private Person persona;
 
     /**
      * Constructor for objects of class Picture
@@ -100,5 +101,10 @@ public class Picture
      */
     public void atardecer(){
         sun.slowMoveVertical(60);
+        persona = new Person();
+        persona.moveHorizontal(-250);
+        persona.moveVertical(30);
+        persona.makeVisible();
+        persona.slowMoveHorizontal(150);
     }
 }
